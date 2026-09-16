8feca90f22243a17dd3cc372ca94449cd8ee00be
atgunasekar0504-devops <atgunasekar0504@gmail.com>
2026-09-16 16:27:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-module "ec2_module" {
+module "ec2-module" {
   source = "./modules/ec2-module"
   instance_name = "tbdp-bi-ec2"
   instance_type = "t3.micro"
   ami_id = "ami-0fe65665c4d6f0de7"
 }
 
-module "dynamodb_module" {
-  source = "./modules/dynamodb_module"
+module "dynamodb-module" {
+  source = "./modules/dynamodb-module"
   table_name = "tbdp-bi-employee-table"
 }
